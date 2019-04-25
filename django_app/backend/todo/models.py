117eceb1714706164c98ee322c1bce02a4dd734b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-25 12:35:00 -0700

    add admin page to manage db

--- django_app/backend/todo/models.py
+++ django_app/backend/todo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Todo(models.Model):
-      title = models.TextField()
-      completed = models.TextField() # will show completed, in progress or not completed
-      dueDate = models.DateTimeField()
 
-      def _str_(self):
+
+class Todo(models.Model):
+    title = models.TextField()
+    completed = models.TextField()  # will show todo/in-progress/done
+    dueDate = models.DateTimeField()
+
+    def _str_(self):
         return self.title
