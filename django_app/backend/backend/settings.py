e9363c080575d80fe8c860675b65c02516c850d9
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-25 12:40:27 -0700

    update settings

--- django_app/backend/backend/settings.py
+++ django_app/backend/backend/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# whitelist front-end
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
