52e1454de26094e532e14004f53167beb9faa242
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-25 12:12:02 -0700

    impletement todo model

--- django_app/backend/todo/models.py
+++ django_app/backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+      title = models.TextField()
+      completed = models.TextField() # will show completed, in progress or not completed
+      dueDate = models.DateTimeField()
+
+      def _str_(self):
+        return self.title
